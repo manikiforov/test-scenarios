0cdeb4540f4b42710e6a3ac1ca9b0f920f49b6be
WebUI <sadmin@just-ai.com>
2018-03-13 13:14:27 +0000

    sadmin@just-ai.com - Edited files: /pageflow/src/function.js

--- pageflow/src/function.js
+++ pageflow/src/function.js
@@ -2,3 +2,9 @@ function pageName(name) {
     var $session = $jsapi.context().session;
     $session.pageName = name;
 }
+
+function rememberLastState() {
+    var $session = $jsapi.context().session;
+    $session.lastState = currentState();
+    log("REMEMBER LAST STATE: " + $session.lastState)
+}
