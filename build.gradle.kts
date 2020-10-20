e6becb1f2408b6cf4e105235f8d5edde28958ff7
s.andronaki@just-ai.com (JAICP) <s.andronaki@just-ai.com>
2020-10-20 15:27:43 +0000

    Create project from template: jaicp-cailapub-ru

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    application
+    kotlin("jvm") version "1.3.61"
+    id("com.github.johnrengelman.shadow") version "5.0.0"
+}
+
+group = "com.justai.jaicf"
+version = "1.0.0"
+
+val jaicf = "0.7.1"
+val logback = "1.2.3"
+val ktor = "1.3.1"
+
+// Main class to run application in JAICP Cloud.
+application {
+    mainClassName = "com.justai.jaicf.template.connections.WebhookConnectionKt"
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    jcenter()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    implementation("ch.qos.logback:logback-classic:$logback")
+
+    implementation("com.justai.jaicf:core:$jaicf")
+    implementation("com.justai.jaicf:mongo:$jaicf")
+    implementation("com.justai.jaicf:jaicp:$jaicf")
+    implementation("com.justai.jaicf:caila:$jaicf")
+
+    implementation("io.ktor:ktor-server-netty:$ktor")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes(
+            mapOf(
+                "Main-Class" to application.mainClassName
+            )
+        )
+    }
+}
+
+tasks.create("stage") {
+    dependsOn("shadowJar")
+}
---
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -0,0 +1,41 @@
+package com.justai.jaicf.template.scenario
+
+import com.justai.jaicf.model.scenario.Scenario
+
+object MainScenario : Scenario() {
+
+    init {
+        state("Start") {
+            globalActivators {
+                regex("/start")
+            }
+            action {
+                reactions.say("Начнём!")
+            }
+        }
+
+        state("Hello") {
+            activators {
+                intent("Hello")
+            }
+
+            action {
+                reactions.say("Привет!")
+            }
+        }
+
+        state("Bye") {
+            activators {
+                intent("Bye")
+            }
+
+            action {
+                reactions.say("Скоро увидимся!")
+            }
+        }
+
+        fallback {
+            reactions.say("Мне пока нечего сказать...")
+        }
+    }
+}
